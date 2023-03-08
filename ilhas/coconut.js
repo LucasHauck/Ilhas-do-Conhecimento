86aee5bfbd82cf8b42251d7d58d44e08b2127b4d
MarkChase3 <103606684+MarkChase3@users.noreply.github.com>
2023-03-08 15:44:51 -0300

    Add files via upload

---
+++ ilhas/coconut.js
@@ -0,0 +1,36 @@
+function coconutUpdate(){
+	sprite('coconutBackground.png', 0, 0, 5, 5)
+	sprite('coconutBasket.png', basket.x, basket.y, 2, 2)
+	sprite('coconut.png', coconut.x, coconut.y)
+	for(let i = 0; i < 10; i++){
+		if(i < coconutPoints){
+			sprite('coconut.png', i*32+15, 5, 0.75, 0.75)
+		} else {
+			sprite('emptyCoconut.png', i*32+15, 5, 0.75, 0.75)
+		}
+	}
+	if(coconutPoints >= 10){
+		room = gameMapUpdate
+	}
+	basket.x += axisX() * 3.4
+	coconut.y += 5
+	if(coconut.y > 420){
+		coconut = {
+			x: random()%350-150+basket.x,
+			y: -32,
+		}
+		coconutPoints = 0
+		basket = {
+			x: centerX('coconutBasket.png'),
+			y: 300
+		}
+		room = gameMapUpdate
+	}
+	if(overlaps(coconut.x, coconut.y, 32, 32, basket.x, basket.y, 32, 32)){
+		coconutPoints++
+		coconut = {
+			x: random()%300-150+basket.x,
+			y: -32,
+		}	
+	}
+}
