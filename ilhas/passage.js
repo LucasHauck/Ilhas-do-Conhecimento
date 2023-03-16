dea8c0f1fb8f6d490e0ce8e1fff373f6344c13fc
MarkChase3 <103606684+MarkChase3@users.noreply.github.com>
2023-03-16 01:41:59 -0300

    Add files via upload

---
+++ ilhas/passage.js
@@ -0,0 +1,8 @@
+function passageUpdate(){
+	render('passageMap.tls', player[5].camX, player[5].camY, 0, 0)
+	playerUpdate(5)
+	if(player[5].y < 0){
+		room = gameMapUpdate
+		mapName = 'gameMap'+subject+'layer1.tls'
+	}
+}
