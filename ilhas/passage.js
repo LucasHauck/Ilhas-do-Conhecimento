ce1568a1d7815abbb4b1a3c1968b1de5e264366e
MarkChase3 <103606684+MarkChase3@users.noreply.github.com>
2023-03-17 19:13:09 -0300

    Delete ilhas directory

--- ilhas/passage.js
+++
@@ -1,8 +0,0 @@
-function passageUpdate(){
-	render('passageMap.tls', player[5].camX, player[5].camY, 0, 0)
-	playerUpdate(5)
-	if(player[5].y < 0){
-		room = gameMapUpdate
-		mapName = 'gameMap'+subject+'layer1.tls'
-	}
-}
--- ilhas/questions.js
+++
@@ -1,4 +0,0 @@
-function questionsUpdate(){
-	sprite('questionsRoomBackground.png', 0, 0, 5, 5)
-	sprite('questionsRoomQuestionBackground.png', centerX()-width('questionsRoomQuestionBackground.png')/2*3, 0, 3, 3)
-}
