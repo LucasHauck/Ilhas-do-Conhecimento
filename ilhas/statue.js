1fdd0bfe299d56aaf78b0705a0e88057fe3beb66
MarkChase3 <103606684+MarkChase3@users.noreply.github.com>
2023-03-20 21:55:04 -0300

    Delete ilhas directory

--- ilhas/statue.js
+++
@@ -1,6 +0,0 @@
-function statueUpdate(statue){
-	if(overlaps(player[0].x, player[0].y, 32, 32, statue.x-8, statue.y-8, 48, 48)){
-		if(isJustPressed(' ')) room = statue.updateMinigame
-		write('Aperte espaço para começar o minigame', centerTextX('Aperte espaço para começar o minigame', '0.5rem slkscr'), 300, 'white', '0.5rem slkscr')
-	}
-}
