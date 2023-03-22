90037e3b78b388ba6febcbf4c7d5093f5a173240
Project - Ilhas do conhecimento <123037984+LucasHauck@users.noreply.github.com>
2023-03-22 08:50:50 -0300

    Delete ilhas directory

--- ilhas/menu.js
+++
@@ -1,7 +0,0 @@
-function menuUpdate(){
-	sprite('menuBackground.png')
-	write('PLAY', centerTextX('PLAY'), centerTextY('PLAY'), "black")
-	if(overlaps(tx, ty, 10, 10, centerTextX('PLAY'), centerTextY('PLAY'), textWidth('PLAY'), fontHeight('PLAY'))){
-		room = islandSelectUpdate
-	}
-}
